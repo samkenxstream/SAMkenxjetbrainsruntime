5c924a789c58a68cd6eb0597bcd551111fe17e03
Alexey Ushakov <alexey.ushakov@jetbrains.com>
2016-08-10 18:33:23 +0300

    8283794: CCE in XRTextRenderer.drawGlyphList and XRMaskFill.MaskFill
    Throwing InvalidPipeException for incompatible surfaces.
    Extracted try/catch logic of SurfaceData cast to particular SurfaceData subclass into convenience method SurfaceData.convertTo().
    Applied the method to XRTextRenderer.drawGlyphList() and XRMaskFill.MaskFill(). Refactored try/catch blocks handling similar cases.

--- src/java.desktop/unix/classes/sun/font/XRTextRenderer.java
+++ src/java.desktop/unix/classes/sun/font/XRTextRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,8 +26,8 @@
 package sun.font;
 
 import sun.awt.*;
-import sun.java2d.InvalidPipeException;
 import sun.java2d.SunGraphics2D;
+import sun.java2d.SurfaceData;
 import sun.java2d.pipe.GlyphListPipe;
 import sun.java2d.xr.*;
 
@@ -62,12 +62,7 @@ public class XRTextRenderer extends GlyphListPipe {
 
         try {
             SunToolkit.awtLock();
-            XRSurfaceData x11sd;
-            try {
-                x11sd = (XRSurfaceData) sg2d.surfaceData;
-            } catch (ClassCastException e) {
-                throw new InvalidPipeException("wrong surface data type: " + sg2d.surfaceData);
-            }
+            XRSurfaceData x11sd = SurfaceData.convertTo(XRSurfaceData.class, sg2d.surfaceData);
             x11sd.validateAsDestination(null, sg2d.getCompClip());
             x11sd.maskBuffer.validateCompositeState(sg2d.composite, sg2d.transform, sg2d.paint, sg2d);
 
