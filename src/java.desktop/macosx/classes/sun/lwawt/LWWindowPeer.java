JetBrains/JetBrainsRuntime

JBR-2533 Popup is not focused on click when switching from another application on macOS

src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1282,39 +1282,9 @@ public class LWWindowPeer
         Window opposite = LWKeyboardFocusManagerPeer.getInstance().
             getCurrentFocusedWindow();
 
-        // Make the owner active window.
-        if (isSimpleWindow()) {
-            LWWindowPeer owner = getOwnerFrameDialog(this);
-
-            // If owner is not natively active, request native
-            // activation on it w/o sending events up to java.
-            if (owner != null && !owner.platformWindow.isActive()) {
-                if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
-                    focusLog.fine("requesting native focus to the owner " + owner);
-                }
-                LWWindowPeer currentActivePeer = currentActive == null ? null :
-                (LWWindowPeer) AWTAccessor.getComponentAccessor().getPeer(
-                        currentActive);
-
-                // Ensure the opposite is natively active and suppress sending events.
-                if (currentActivePeer != null && currentActivePeer.platformWindow.isActive()) {
-                    if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
-                        focusLog.fine("the opposite is " + currentActivePeer);
-                    }
-                    currentActivePeer.skipNextFocusChange = true;
-                }
-                owner.skipNextFocusChange = true;
-
-                owner.platformWindow.requestWindowFocus();
-            }
-
-            // DKFM will synthesize all the focus/activation events correctly.
-            changeFocusedWindow(true, opposite, lightweightRequest);
-            return true;
-
         // In case the toplevel is active but not focused, change focus directly,
         // as requesting native focus on it will not have effect.
-        } else if (getTarget() == currentActive && !getTarget().isFocused()) {
+        if (getTarget() == currentActive && !getTarget().isFocused()) {
             changeFocusedWindow(true, opposite, lightweightRequest);
             return true;
         }
@@ -1401,7 +1371,8 @@ public class LWWindowPeer
         // - when the opposite (gaining focus) window is an owned/owner window.
         // - for a simple window in any case.
         if (!becomesFocused &&
-            (isGrabbing() || this.isOneOfOwnersOf(grabbingWindow)))
+            (isGrabbing() || this.isOneOfOwnersOf(grabbingWindow)) &&
+            getOwnerFrameDialog(AWTAccessor.getComponentAccessor().getPeer(opposite)) != this)
         {
             if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
                 focusLog.fine("ungrabbing on " + grabbingWindow);

test/jdk/jb/java/awt/Focus/PopupIncomingFocusTest.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2533 Popup is not focused on click when switching from another application on macOS
+ * @key headful
+ */
+
+public class PopupIncomingFocusTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> result = new CompletableFuture<>();
+    private static Robot robot;
+    private static Process otherProcess;
+    private static JFrame frame;
+    private static JWindow popup;
+    private static JTextField field;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoWaitForIdle(true);
+        try {
+            launchProcessWithWindow();
+            SwingUtilities.invokeLater(PopupIncomingFocusTest::init);
+            initFinished.get(15, TimeUnit.SECONDS);
+
+            clickAt(400, 100); // other process' window
+            clickAt(field);
+            pressEnter();
+
+            result.get(15, TimeUnit.SECONDS);
+        }
+        finally {
+            SwingUtilities.invokeLater(PopupIncomingFocusTest::shutdown);
+        }
+    }
+
+    private static void init() {
+        frame = new JFrame();
+        frame.setBounds(50, 50, 200, 100);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                popup.setVisible(true);
+            }
+        });
+
+        field = new JTextField(10);
+        field.getCaret().setBlinkRate(0); // prevent caret blink timer from keeping event thread running
+        field.addActionListener(e -> result.complete(Boolean.TRUE));
+
+        popup = new JWindow(frame);
+        popup.setType(Window.Type.POPUP);
+        popup.add(field);
+        popup.pack();
+        popup.setLocation(50, 200);
+        popup.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                initFinished.complete(Boolean.TRUE);
+            }
+        });
+
+        frame.setVisible(true);
+    }
+
+    private static void shutdown() {
+        if (frame != null) frame.dispose();
+        if (popup != null) popup.dispose();
+        if (otherProcess != null) otherProcess.destroyForcibly();
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickAt(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+
+    private static void pressEnter() {
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+
+    private static void launchProcessWithWindow() throws Exception {
+        String javaPath = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        File tmpFile = File.createTempFile("PopupIncomingFocusTest", ".java");
+        tmpFile.deleteOnExit();
+        Files.writeString(tmpFile.toPath(), "import javax.swing.*;\n" +
+                "import java.awt.event.*;\n" +
+                "\n" +
+                "public class TestWindow {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        SwingUtilities.invokeLater(() -> {\n" +
+                "            JFrame f = new JFrame();\n" +
+                "            f.addWindowListener(new WindowAdapter() {\n" +
+                "                @Override\n" +
+                "                public void windowOpened(WindowEvent e) {\n" +
+                "                    System.out.println();\n" +
+                "                }\n" +
+                "            });\n" +
+                "            f.setBounds(300, 50, 200, 100);\n" +
+                "            f.setVisible(true);\n" +
+                "        });\n" +
+                "    }\n" +
+                "}\n");
+        otherProcess = Runtime.getRuntime().exec(new String[]{javaPath, tmpFile.getAbsolutePath()});
+        if (otherProcess.getInputStream().read() == -1) {
+            throw new RuntimeException("Error starting process");
+        }
+    }
+}
+

samkenxstream/SAMkenxjetbrainsruntime

(cherry picked from commits d9ff1512115334f2da08bd93ca3ff503815db982, 67b174dc8c4e13e601443e82c59d7ff95e1b6bf3, 72b0add80c827072c6834649bcdf234f7713fd3f, 21af1eba85693e628ee24c2e0c28df15b837829c, 2f1d317d8711f1ddf6c24d3bf066132c42a7aa0f, 6dd334f9f056060392cebace82ec852478204747, cd863bac0de121f667265fd122cc45952cf50f46, 010f6fc9514e9200f6a14ca0af065f1f975b4611, parts of 7d5ac56b6c8044059438ff9131ce7826bb5a891e, cd6dd5c3cf8556f97f3113cb7d615a92393b57bf, e8bbd8ffdd90f57cd12d7d7e89188be97ee4be0b)

src/java.desktop/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1268,39 +1268,9 @@ public class LWWindowPeer
         Window opposite = LWKeyboardFocusManagerPeer.getInstance().
             getCurrentFocusedWindow();
 
-        // Make the owner active window.
-        if (isSimpleWindow()) {
-            LWWindowPeer owner = getOwnerFrameDialog(this);
-
-            // If owner is not natively active, request native
-            // activation on it w/o sending events up to java.
-            if (owner != null && !owner.platformWindow.isActive()) {
-                if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
-                    focusLog.fine("requesting native focus to the owner " + owner);
-                }
-                LWWindowPeer currentActivePeer = currentActive == null ? null :
-                (LWWindowPeer) AWTAccessor.getComponentAccessor().getPeer(
-                        currentActive);
-
-                // Ensure the opposite is natively active and suppress sending events.
-                if (currentActivePeer != null && currentActivePeer.platformWindow.isActive()) {
-                    if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
-                        focusLog.fine("the opposite is " + currentActivePeer);
-                    }
-                    currentActivePeer.skipNextFocusChange = true;
-                }
-                owner.skipNextFocusChange = true;
-
-                owner.platformWindow.requestWindowFocus();
-            }
-
-            // DKFM will synthesize all the focus/activation events correctly.
-            changeFocusedWindow(true, opposite);
-            return true;
-
         // In case the toplevel is active but not focused, change focus directly,
         // as requesting native focus on it will not have effect.
-        } else if (getTarget() == currentActive && !getTarget().hasFocus()) {
+        if (getTarget() == currentActive && !getTarget().hasFocus()) {
 
             changeFocusedWindow(true, opposite);
             return true;
@@ -1381,7 +1351,8 @@ public class LWWindowPeer
         // - when the opposite (gaining focus) window is an owned/owner window.
         // - for a simple window in any case.
         if (!becomesFocused &&
-            (isGrabbing() || this.isOneOfOwnersOf(grabbingWindow)))
+            (isGrabbing() || this.isOneOfOwnersOf(grabbingWindow)) &&
+            (opposite == null || getOwnerFrameDialog(AWTAccessor.getComponentAccessor().getPeer(opposite)) != this))
         {
             if (focusLog.isLoggable(PlatformLogger.Level.FINE)) {
                 focusLog.fine("ungrabbing on " + grabbingWindow);

test/jdk/jb/java/awt/Focus/PopupIncomingFocusTest.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2533 Popup is not focused on click when switching from another application on macOS
+ * @key headful
+ */
+
+public class PopupIncomingFocusTest {
+    private static final CompletableFuture<Boolean> windowOpened = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> popupOpened = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> result = new CompletableFuture<>();
+    private static Robot robot;
+    private static Process otherProcess;
+    private static JFrame frame;
+    private static JButton button;
+    private static JWindow popup;
+    private static JTextField field;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoWaitForIdle(true);
+        try {
+            SwingUtilities.invokeAndWait(PopupIncomingFocusTest::init);
+            windowOpened.get(10, TimeUnit.SECONDS);
+            launchProcessWithWindow();
+            clickAt(button);
+            popupOpened.get(10, TimeUnit.SECONDS);
+            clickAt(400, 100); // other process' window
+            clickAt(field);
+            pressEnter();
+            result.get(10, TimeUnit.SECONDS);
+        }
+        finally {
+            SwingUtilities.invokeAndWait(PopupIncomingFocusTest::shutdown);
+        }
+    }
+
+    private static void init() {
+        button = new JButton("Open popup");
+        button.addActionListener(e -> {
+            popup.setVisible(true);
+        });
+
+        frame = new JFrame();
+        frame.add(button);
+        frame.setBounds(50, 50, 200, 100);
+        frame.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                windowOpened.complete(Boolean.TRUE);
+            }
+        });
+
+        field = new JTextField(10);
+        field.getCaret().setBlinkRate(0); // prevent caret blink timer from keeping event thread running
+        field.addActionListener(e -> result.complete(Boolean.TRUE));
+
+        popup = new JWindow(frame);
+        popup.setType(Window.Type.POPUP);
+        popup.add(field);
+        popup.pack();
+        popup.setLocation(50, 200);
+        popup.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                popupOpened.complete(Boolean.TRUE);
+            }
+        });
+
+        frame.setVisible(true);
+    }
+
+    private static void shutdown() {
+        if (frame != null) frame.dispose();
+        if (otherProcess != null) otherProcess.destroyForcibly();
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.delay(1000); // needed for GNOME, to give it some time to update internal state after window showing
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickAt(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+
+    private static void pressEnter() {
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+
+    private static void launchProcessWithWindow() throws Exception {
+        String javaPath = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        File tmpFile = File.createTempFile("PopupIncomingFocusTest", ".java");
+        tmpFile.deleteOnExit();
+        Files.writeString(tmpFile.toPath(), "import javax.swing.*;\n" +
+                "import java.awt.event.*;\n" +
+                "\n" +
+                "public class TestWindow {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        SwingUtilities.invokeLater(() -> {\n" +
+                "            JFrame f = new JFrame();\n" +
+                "            f.addWindowFocusListener(new WindowAdapter() {\n" +
+                "                @Override\n" +
+                "                public void windowGainedFocus(WindowEvent e) {\n" +
+                "                    System.out.println();\n" +
+                "                }\n" +
+                "            });\n" +
+                "            f.setBounds(300, 50, 200, 100);\n" +
+                "            f.setVisible(true);\n" +
+                "        });\n" +
+                "    }\n" +
+                "}\n");
+        otherProcess = Runtime.getRuntime().exec(new String[]{javaPath, tmpFile.getAbsolutePath()});
+        if (otherProcess.getInputStream().read() == -1) {
+            throw new RuntimeException("Error starting process");
+        }
+    }
+}