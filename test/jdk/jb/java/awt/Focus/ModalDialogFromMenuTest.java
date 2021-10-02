JetBrains/JetBrainsRuntime

JBR-2712 Typeahead mechanism doesn't work on Windows

test/jdk/jb/java/awt/Focus/ModalDialogFromMenuTest.java
@@ -1,0 +1,126 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Test for the case which was broken during development for JBR-2712 (Typeahead mechanism doesn't work on Windows)
+ * @key headful
+ */
+
+public class ModalDialogFromMenuTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> menuItemShown = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInDialog = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+    private static JMenuItem menuItem;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50);
+        try {
+            SwingUtilities.invokeAndWait(ModalDialogFromMenuTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            rightClickOn(frameField);
+            menuItemShown.get(10, TimeUnit.SECONDS);
+            clickOn(menuItem);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInDialog.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(ModalDialogFromMenuTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("ModalDialogFromMenuTest");
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        JPopupMenu menu = new JPopupMenu();
+        menuItem = new JMenuItem(new AbstractAction("open dialog") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDialog d = new JDialog(frame, true);
+                JTextField dialogField = new JTextField(10);
+                dialogField.getDocument().addDocumentListener(new DocumentListener() {
+                    @Override
+                    public void insertUpdate(DocumentEvent e) {
+                        typedInDialog.complete(true);
+                    }
+
+                    @Override
+                    public void removeUpdate(DocumentEvent e) {}
+
+                    @Override
+                    public void changedUpdate(DocumentEvent e) {}
+                });
+                d.add(dialogField);
+                d.pack();
+                d.setVisible(true);
+            }
+        });
+        menuItem.addHierarchyListener(e -> {
+            if (menuItem.isShowing()) menuItemShown.complete(true);
+        });
+        menu.add(menuItem);
+        frameField.setComponentPopupMenu(menu);
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y, int buttons) {
+        robot.mouseMove(x, y);
+        robot.mousePress(buttons);
+        robot.mouseRelease(buttons);
+    }
+
+    private static void clickOn(Component component, int buttons) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2, buttons);
+    }
+
+    private static void clickOn(Component component) {
+        clickOn(component, InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void rightClickOn(Component component) {
+        clickOn(component, InputEvent.BUTTON3_DOWN_MASK);
+    }
+}

test/jdk/jb/java/awt/Focus/TypeaheadRequestFocusTest.java
@@ -1,0 +1,115 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2712 Typeahead mechanism doesn't work on Windows
+ * @key headful
+ */
+
+public class TypeaheadRequestFocusTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInPopup = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+    private static JTextField windowField;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50);
+        try {
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            clickOn(frameField);
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::showPopup);
+            pressAndRelease(KeyEvent.VK_ENTER);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInPopup.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("TypeaheadRequestFocusTest");
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        frameField.addActionListener(e -> {
+            LockSupport.parkNanos(1_000_000_000L);
+            windowField.requestFocus();
+        });
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void showPopup() {
+        JWindow window = new JWindow(frame);
+        windowField = new JTextField(20);
+        windowField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                typedInPopup.complete(true);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {}
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {}
+        });
+        window.add(windowField);
+        window.pack();
+        window.setAutoRequestFocus(false);
+        window.setVisible(true);
+        window.setAutoRequestFocus(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickOn(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+}

test/jdk/jb/java/awt/Focus/TypeaheadSetVisibleTest.java
@@ -1,0 +1,107 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2712 Typeahead mechanism doesn't work on Windows
+ * @key headful
+ */
+
+public class TypeaheadSetVisibleTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInPopup = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50);
+        try {
+            SwingUtilities.invokeAndWait(TypeaheadSetVisibleTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            clickOn(frameField);
+            pressAndRelease(KeyEvent.VK_ENTER);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInPopup.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(TypeaheadSetVisibleTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("TypeaheadSetVisibleTest");
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        frameField.addActionListener(e -> {
+            LockSupport.parkNanos(1_000_000_000L);
+            JWindow window = new JWindow(frame);
+            JTextField windowField = new JTextField(20);
+            windowField.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    typedInPopup.complete(true);
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {}
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {}
+            });
+            window.add(windowField);
+            window.pack();
+            window.setVisible(true);
+        });
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickOn(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+}

samkenxstream/SAMkenxjetbrainsruntime

(cherry picked from commits 1a9838082e3eb48d43e6bac6a412463923173fc7, f5b62228352bacb1628d232f0af5d8d387b6bf97, acd7e3b2da62332992f986903271a9eb7b1a2597, cd6dd5c3cf8556f97f3113cb7d615a92393b57bf(partially), e8bbd8ffdd90f57cd12d7d7e89188be97ee4be0b(partially), 37901295e1f6084b423c73df2b700552e5a4f519)

test/jdk/jb/java/awt/Focus/ModalDialogFromMenuTest.java
@@ -1,0 +1,127 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Test for the case which was broken during development for JBR-2712 (Typeahead mechanism doesn't work on Windows)
+ * @key headful
+ */
+
+public class ModalDialogFromMenuTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> menuItemShown = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInDialog = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+    private static JMenuItem menuItem;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50); // ensure different timestamps for key events (can impact typeahead logic)
+        try {
+            SwingUtilities.invokeAndWait(ModalDialogFromMenuTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            rightClickOn(frameField);
+            menuItemShown.get(10, TimeUnit.SECONDS);
+            clickOn(menuItem);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInDialog.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(ModalDialogFromMenuTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("ModalDialogFromMenuTest");
+        frame.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        JPopupMenu menu = new JPopupMenu();
+        menuItem = new JMenuItem(new AbstractAction("open dialog") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JDialog d = new JDialog(frame, true);
+                JTextField dialogField = new JTextField(10);
+                dialogField.getDocument().addDocumentListener(new DocumentListener() {
+                    @Override
+                    public void insertUpdate(DocumentEvent e) {
+                        typedInDialog.complete(true);
+                    }
+
+                    @Override
+                    public void removeUpdate(DocumentEvent e) {}
+
+                    @Override
+                    public void changedUpdate(DocumentEvent e) {}
+                });
+                d.add(dialogField);
+                d.pack();
+                d.setVisible(true);
+            }
+        });
+        menuItem.addHierarchyListener(e -> {
+            if (menuItem.isShowing()) menuItemShown.complete(true);
+        });
+        menu.add(menuItem);
+        frameField.setComponentPopupMenu(menu);
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y, int buttons) {
+        robot.delay(1000); // needed for GNOME, to give it some time to update internal state after window showing
+        robot.mouseMove(x, y);
+        robot.mousePress(buttons);
+        robot.mouseRelease(buttons);
+    }
+
+    private static void clickOn(Component component, int buttons) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2, buttons);
+    }
+
+    private static void clickOn(Component component) {
+        clickOn(component, InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void rightClickOn(Component component) {
+        clickOn(component, InputEvent.BUTTON3_DOWN_MASK);
+    }
+}

test/jdk/jb/java/awt/Focus/TypeaheadRequestFocusTest.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2712 Typeahead mechanism doesn't work on Windows
+ * @key headful
+ */
+
+public class TypeaheadRequestFocusTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInPopup = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+    private static JTextField windowField;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50); // ensure different timestamps for key events (can impact typeahead logic)
+        try {
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            clickOn(frameField);
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::showPopup);
+            robot.delay(1000);
+            pressAndRelease(KeyEvent.VK_ENTER);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInPopup.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(TypeaheadRequestFocusTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("TypeaheadRequestFocusTest");
+        frame.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        frameField.addActionListener(e -> {
+            LockSupport.parkNanos(1_000_000_000L);
+            windowField.requestFocus();
+        });
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void showPopup() {
+        JWindow window = new JWindow(frame);
+        windowField = new JTextField(20);
+        windowField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                typedInPopup.complete(true);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {}
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {}
+        });
+        window.add(windowField);
+        window.pack();
+        window.setAutoRequestFocus(false);
+        window.setVisible(true);
+        window.setAutoRequestFocus(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.delay(1000); // needed for GNOME, to give it some time to update internal state after window showing
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickOn(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+}

test/jdk/jb/java/awt/Focus/TypeaheadSetVisibleTest.java
@@ -1,0 +1,108 @@
+/*
+ * Copyright 2000-2020 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @test
+ * @summary Regression test for JBR-2712 Typeahead mechanism doesn't work on Windows
+ * @key headful
+ */
+
+public class TypeaheadSetVisibleTest {
+    private static final CompletableFuture<Boolean> initFinished = new CompletableFuture<>();
+    private static final CompletableFuture<Boolean> typedInPopup = new CompletableFuture<>();
+    private static Robot robot;
+    private static JFrame frame;
+    private static JTextField frameField;
+
+    public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(50); // ensure different timestamps for key events (can impact typeahead logic)
+        try {
+            SwingUtilities.invokeAndWait(TypeaheadSetVisibleTest::initUI);
+            initFinished.get(10, TimeUnit.SECONDS);
+            clickOn(frameField);
+            pressAndRelease(KeyEvent.VK_ENTER);
+            pressAndRelease(KeyEvent.VK_A);
+            typedInPopup.get(10, TimeUnit.SECONDS);
+        } finally {
+            SwingUtilities.invokeAndWait(TypeaheadSetVisibleTest::disposeUI);
+        }
+    }
+
+    private static void initUI() {
+        frame = new JFrame("TypeaheadSetVisibleTest");
+        frame.addWindowFocusListener(new WindowAdapter() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                initFinished.complete(true);
+            }
+        });
+        frameField = new JTextField(20);
+        frameField.addActionListener(e -> {
+            LockSupport.parkNanos(1_000_000_000L);
+            JWindow window = new JWindow(frame);
+            JTextField windowField = new JTextField(20);
+            windowField.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    typedInPopup.complete(true);
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {}
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {}
+            });
+            window.add(windowField);
+            window.pack();
+            window.setVisible(true);
+        });
+        frame.add(frameField);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void disposeUI() {
+        if (frame != null) frame.dispose();
+    }
+
+    private static void pressAndRelease(int keyCode) {
+        robot.keyPress(keyCode);
+        robot.keyRelease(keyCode);
+    }
+
+    private static void clickAt(int x, int y) {
+        robot.delay(1000); // needed for GNOME, to give it some time to update internal state after window showing
+        robot.mouseMove(x, y);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+    private static void clickOn(Component component) {
+        Point location = component.getLocationOnScreen();
+        clickAt(location.x + component.getWidth() / 2, location.y + component.getHeight() / 2);
+    }
+}