JetBrains/JetBrainsRuntime

JBR-2050 Issue with keycap emojis

src/java.desktop/share/classes/sun/font/FontUtilities.java
@@ -132,7 +132,7 @@ public final class FontUtilities {
      * where the caller interprets 'layout' to mean any case where
      * one 'char' (ie the java type char) does not map to one glyph
      */
-    public static final int MAX_LAYOUT_CHARCODE = 0x206F;
+    public static final int MAX_LAYOUT_CHARCODE = 0x20F0;
 
     /**
      * Calls the private getFont2D() method in java.awt.Font objects.
@@ -297,6 +297,9 @@ public final class FontUtilities {
             return true;
         }
         else if (code >= 0x206a && code <= 0x206f) { // directional control
+            return true;
+        }
+        else if (code >= 0x20d0) { // U+20D0 - U+20D0 combining diacritical marks for symbols
             return true;
         }
         return false;

samkenxstream/SAMkenxjetbrainsruntime

port from JBR 11 to JBR 15 (cherry picked from commit ae91e1d7f14cee86630d3229c96cad6e59070e5d)
cherry picked from commit d3018a18377aadf66a0ff5b3c2421b2368061da9

src/java.desktop/share/classes/sun/font/FontUtilities.java
@@ -138,7 +138,7 @@ public final class FontUtilities {
      * where the caller interprets 'layout' to mean any case where
      * one 'char' (ie the java type char) does not map to one glyph
      */
-    public static final int MAX_LAYOUT_CHARCODE = 0x206F;
+    public static final int MAX_LAYOUT_CHARCODE = 0x20F0;
 
     /**
      * Calls the private getFont2D() method in java.awt.Font objects.
@@ -303,6 +303,9 @@ public final class FontUtilities {
             return true;
         }
         else if (code >= 0x206a && code <= 0x206f) { // directional control
+            return true;
+        }
+        else if (code >= 0x20d0) { // U+20D0 - U+20F0 combining diacritical marks for symbols
             return true;
         }
         return false;