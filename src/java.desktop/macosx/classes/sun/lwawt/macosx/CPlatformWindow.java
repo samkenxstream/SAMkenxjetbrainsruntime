JetBrains/JetBrainsRuntime

JBR-3676 WINDOW_ACTIVATED/DEACTIVATED events sent to a frame when child window closes on macOS

src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -96,6 +96,7 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
     private static native void nativeSetNSWindowMinMax(long nsWindowPtr, double minW, double minH, double maxW, double maxH);
     private static native void nativePushNSWindowToBack(long nsWindowPtr);
     private static native void nativePushNSWindowToFront(long nsWindowPtr, boolean wait);
+    private static native void nativeHideWindow(long nsWindowPtr, boolean wait);
     private static native void nativeSetNSWindowTitle(long nsWindowPtr, String title);
     private static native void nativeRevalidateNSWindowShadow(long nsWindowPtr);
     private static native void nativeSetNSWindowMinimizedIcon(long nsWindowPtr, long nsImage);
@@ -760,7 +761,9 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
         // Actually show or hide the window
         LWWindowPeer blocker = (peer == null)? null : peer.getBlocker();
-        if (visible && delayShowing()) {
+        if (!visible) {
+            execute(ptr -> AWTThreading.executeWaitToolkit(wait -> nativeHideWindow(ptr, wait)));
+        } else if (delayShowing()) {
             if (blocker == null) {
                 Window focusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
                 LWWindowPeer focusedWindowBlocker = getBlockerFor(focusedWindow);
@@ -770,43 +773,34 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
                     owner.execute(CWrapper.NSWindow::orderFront);
                 }
             }
-        } else if (blocker == null || !visible) {
+        } else if (blocker == null) {
             // If it ain't blocked, or is being hidden, go regular way
-            if (visible) {
-                boolean isPopup = (target.getType() == Window.Type.POPUP);
-                execute(ptr -> {
+            boolean isPopup = (target.getType() == Window.Type.POPUP);
+            execute(ptr -> {
 
-                    boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
-                    if (!isKeyWindow) {
-                        logger.fine("setVisible: makeKeyAndOrderFront");
-                        CWrapper.NSWindow.makeKeyAndOrderFront(ptr);
-                    } else {
-                        logger.fine("setVisible: orderFront");
-                        CWrapper.NSWindow.orderFront(ptr);
+                boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
+                if (!isKeyWindow) {
+                    logger.fine("setVisible: makeKeyAndOrderFront");
+                    CWrapper.NSWindow.makeKeyAndOrderFront(ptr);
+                } else {
+                    logger.fine("setVisible: orderFront");
+                    CWrapper.NSWindow.orderFront(ptr);
+                }
+
+                if (owner != null
+                        && owner.getPeer() instanceof LWLightweightFramePeer) {
+                    LWLightweightFramePeer peer =
+                            (LWLightweightFramePeer) owner.getPeer();
+
+                    long ownerWindowPtr = peer.getOverriddenWindowHandle();
+                    if (ownerWindowPtr != 0) {
+                        //Place window above JavaFX stage
+                        CWrapper.NSWindow.addChildWindow(
+                                ownerWindowPtr, ptr,
+                                CWrapper.NSWindow.NSWindowAbove);
                     }
-
-                    if (owner != null
-                            && owner.getPeer() instanceof LWLightweightFramePeer) {
-                        LWLightweightFramePeer peer =
-                                (LWLightweightFramePeer) owner.getPeer();
-
-                        long ownerWindowPtr = peer.getOverriddenWindowHandle();
-                        if (ownerWindowPtr != 0) {
-                            //Place window above JavaFX stage
-                            CWrapper.NSWindow.addChildWindow(
-                                    ownerWindowPtr, ptr,
-                                    CWrapper.NSWindow.NSWindowAbove);
-                        }
-                    }
-                });
-            } else {
-                execute(ptr->{
-                    // immediately hide the window
-                    CWrapper.NSWindow.orderOut(ptr);
-                    // process the close
-                    CWrapper.NSWindow.close(ptr);
-                });
-            }
+                }
+            });
         } else {
             // otherwise, put it in a proper z-order
             CPlatformWindow bw

src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -1516,6 +1516,38 @@ JNF_COCOA_EXIT(env);
 
 /*
  * Class:     sun_lwawt_macosx_CPlatformWindow
+ * Method:    nativeHideWindow
+ * Signature: (JZ)V
+ */
+JNIEXPORT void JNICALL Java_sun_lwawt_macosx_CPlatformWindow_nativeHideWindow
+(JNIEnv *env, jclass clazz, jlong windowPtr, jboolean wait)
+{
+JNF_COCOA_ENTER(env);
+
+    NSWindow *nsWindow = OBJC(windowPtr);
+    [ThreadUtilities performOnMainThreadWaiting:(BOOL)wait block:^(){
+        if (nsWindow.keyWindow) {
+            // When 'windowDidResignKey' is called during 'orderOut', current key window
+            // is reported as 'nil', so it's impossible to create WINDOW_FOCUS_LOST event
+            // with correct 'opposite' window.
+            // So, as a workaround, we perform focus transfer to a parent window explicitly here.
+            NSWindow *parentWindow = nsWindow;
+            while ((parentWindow = ((AWTWindow*)parentWindow.delegate).ownerWindow.nsWindow) != nil) {
+                if (parentWindow.canBecomeKeyWindow) {
+                    [parentWindow makeKeyWindow];
+                    break;
+                }
+            }
+        }
+        [nsWindow orderOut:nsWindow];
+        [nsWindow close];
+    }];
+
+JNF_COCOA_EXIT(env);
+}
+
+/*
+ * Class:     sun_lwawt_macosx_CPlatformWindow
  * Method:    nativeSetNSWindowTitle
  * Signature: (JLjava/lang/String;)V
  */

samkenxstream/SAMkenxjetbrainsruntime

(cherry-picked from commit 824f9ebec39cd518f3ec085c068ac6d6d4c22ed9)

src/java.desktop/macosx/classes/sun/lwawt/macosx/CPlatformWindow.java
@@ -87,6 +87,7 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
     private static native void nativeSetNSWindowMinMax(long nsWindowPtr, double minW, double minH, double maxW, double maxH);
     private static native void nativePushNSWindowToBack(long nsWindowPtr);
     private static native void nativePushNSWindowToFront(long nsWindowPtr, boolean wait);
+    private static native void nativeHideWindow(long nsWindowPtr, boolean wait);
     private static native void nativeSetNSWindowTitle(long nsWindowPtr, String title);
     private static native void nativeRevalidateNSWindowShadow(long nsWindowPtr);
     private static native void nativeSetNSWindowMinimizedIcon(long nsWindowPtr, long nsImage);
@@ -714,7 +715,9 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
 
         // Actually show or hide the window
         LWWindowPeer blocker = (peer == null)? null : peer.getBlocker();
-        if (visible && delayShowing()) {
+        if (!visible) {
+            execute(ptr -> AWTThreading.executeWaitToolkit(wait -> nativeHideWindow(ptr, wait)));
+        } else if (delayShowing()) {
             if (blocker == null) {
                 Window focusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
                 LWWindowPeer focusedWindowBlocker = getBlockerFor(focusedWindow);
@@ -724,51 +727,42 @@ public class CPlatformWindow extends CFRetainedResource implements PlatformWindo
                     owner.execute(CWrapper.NSWindow::orderFront);
                 }
             }
-        } else if (blocker == null || !visible) {
+        } else if (blocker == null) {
             // If it ain't blocked, or is being hidden, go regular way
-            if (visible) {
-                contentView.execute(viewPtr -> {
-                    execute(ptr -> CWrapper.NSWindow.makeFirstResponder(ptr,
-                                                                        viewPtr));
-                });
+            contentView.execute(viewPtr -> {
+                execute(ptr -> CWrapper.NSWindow.makeFirstResponder(ptr,
+                                                                    viewPtr));
+            });
 
-                boolean isPopup = (target.getType() == Window.Type.POPUP);
-                execute(ptr -> {
-                    if (isPopup) {
-                        // Popups in applets don't activate applet's process
-                        CWrapper.NSWindow.orderFrontRegardless(ptr);
-                    } else {
-                        CWrapper.NSWindow.orderFront(ptr);
+            boolean isPopup = (target.getType() == Window.Type.POPUP);
+            execute(ptr -> {
+                if (isPopup) {
+                    // Popups in applets don't activate applet's process
+                    CWrapper.NSWindow.orderFrontRegardless(ptr);
+                } else {
+                    CWrapper.NSWindow.orderFront(ptr);
+                }
+
+                boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
+                if (!isKeyWindow) {
+                    logger.fine("setVisible: makeKeyWindow");
+                    CWrapper.NSWindow.makeKeyWindow(ptr);
+                }
+
+                if (owner != null
+                        && owner.getPeer() instanceof LWLightweightFramePeer) {
+                    LWLightweightFramePeer peer =
+                            (LWLightweightFramePeer) owner.getPeer();
+
+                    long ownerWindowPtr = peer.getOverriddenWindowHandle();
+                    if (ownerWindowPtr != 0) {
+                        //Place window above JavaFX stage
+                        CWrapper.NSWindow.addChildWindow(
+                                ownerWindowPtr, ptr,
+                                CWrapper.NSWindow.NSWindowAbove);
                     }
-
-                    boolean isKeyWindow = CWrapper.NSWindow.isKeyWindow(ptr);
-                    if (!isKeyWindow) {
-                        logger.fine("setVisible: makeKeyWindow");
-                        CWrapper.NSWindow.makeKeyWindow(ptr);
-                    }
-
-                    if (owner != null
-                            && owner.getPeer() instanceof LWLightweightFramePeer) {
-                        LWLightweightFramePeer peer =
-                                (LWLightweightFramePeer) owner.getPeer();
-
-                        long ownerWindowPtr = peer.getOverriddenWindowHandle();
-                        if (ownerWindowPtr != 0) {
-                            //Place window above JavaFX stage
-                            CWrapper.NSWindow.addChildWindow(
-                                    ownerWindowPtr, ptr,
-                                    CWrapper.NSWindow.NSWindowAbove);
-                        }
-                    }
-                });
-            } else {
-                execute(ptr->{
-                    // immediately hide the window
-                    CWrapper.NSWindow.orderOut(ptr);
-                    // process the close
-                    CWrapper.NSWindow.close(ptr);
-                });
-            }
+                }
+            });
         } else {
             // otherwise, put it in a proper z-order
             CPlatformWindow bw

src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -1552,6 +1552,38 @@ JNI_COCOA_EXIT(env);
 
 /*
  * Class:     sun_lwawt_macosx_CPlatformWindow
+ * Method:    nativeHideWindow
+ * Signature: (JZ)V
+ */
+JNIEXPORT void JNICALL Java_sun_lwawt_macosx_CPlatformWindow_nativeHideWindow
+(JNIEnv *env, jclass clazz, jlong windowPtr, jboolean wait)
+{
+JNI_COCOA_ENTER(env);
+
+    NSWindow *nsWindow = OBJC(windowPtr);
+    [ThreadUtilities performOnMainThreadWaiting:(BOOL)wait block:^(){
+        if (nsWindow.keyWindow) {
+            // When 'windowDidResignKey' is called during 'orderOut', current key window
+            // is reported as 'nil', so it's impossible to create WINDOW_FOCUS_LOST event
+            // with correct 'opposite' window.
+            // So, as a workaround, we perform focus transfer to a parent window explicitly here.
+            NSWindow *parentWindow = nsWindow;
+            while ((parentWindow = ((AWTWindow*)parentWindow.delegate).ownerWindow.nsWindow) != nil) {
+                if (parentWindow.canBecomeKeyWindow) {
+                    [parentWindow makeKeyWindow];
+                    break;
+                }
+            }
+        }
+        [nsWindow orderOut:nsWindow];
+        [nsWindow close];
+    }];
+
+JNI_COCOA_EXIT(env);
+}
+
+/*
+ * Class:     sun_lwawt_macosx_CPlatformWindow
  * Method:    nativeSetNSWindowTitle
  * Signature: (JLjava/lang/String;)V
  */