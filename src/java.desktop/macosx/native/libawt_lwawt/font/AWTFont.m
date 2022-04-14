JetBrains/JetBrainsRuntime

JBR-4072 Migrate font fallback implementation on macOS to cascade lists

src/java.desktop/macosx/native/libawt_lwawt/font/AWTFont.m
@@ -3350,3 +3350,61 @@ Java_sun_awt_FontDescriptor_initIDs
 {
 }
 #endif
+
+/*
+ * Class:     sun_font_CFont
+ * Method:    getCascadeList
+ * Signature: (JLjava/util/ArrayList;)V
+ */
+JNIEXPORT void JNICALL
+Java_sun_font_CFont_getCascadeList
+    (JNIEnv *env, jclass cls, jlong awtFontPtr, jobject arrayListOfString)
+{
+JNI_COCOA_ENTER(env);
+    jclass alc = (*env)->FindClass(env, "java/util/ArrayList");
+    if (alc == NULL) return;
+    jmethodID addMID = (*env)->GetMethodID(env, alc, "add", "(Ljava/lang/Object;)Z");
+    if (addMID == NULL) return;
+
+    CFIndex i;
+    AWTFont *awtFont = (AWTFont *)jlong_to_ptr(awtFontPtr);
+    NSFont* nsFont = awtFont->fFont;
+#ifdef DEBUG
+    CFStringRef base = CTFontCopyFullName((CTFontRef)nsFont);
+    NSLog(@"BaseFont is : %@", (NSString*)base);
+    CFRelease(base);
+#endif
+    bool anotherBaseFont = false;
+    if (awtFont->fFallbackBase != nil) {
+        nsFont = awtFont->fFallbackBase;
+        anotherBaseFont = true;
+    }
+    CTFontRef font = (CTFontRef)nsFont;
+    CFArrayRef codes = CFLocaleCopyISOLanguageCodes();
+
+    CFArrayRef fds = CTFontCopyDefaultCascadeListForLanguages(font, codes);
+    CFRelease(codes);
+    CFIndex cnt = CFArrayGetCount(fds);
+    for (i= anotherBaseFont ? -1 : 0; i<cnt; i++) {
+        CFStringRef fontname;
+        if (i < 0) {
+            fontname = CTFontCopyPostScriptName(font);
+        } else {
+            CTFontDescriptorRef ref = CFArrayGetValueAtIndex(fds, i);
+            fontname = CTFontDescriptorCopyAttribute(ref, kCTFontNameAttribute);
+        }
+#ifdef DEBUG
+        NSLog(@"Font is : %@", (NSString*)fontname);
+#endif
+        jstring jFontName = (jstring)NSStringToJavaString(env, fontname);
+        CFRelease(fontname);
+        (*env)->CallBooleanMethod(env, arrayListOfString, addMID, jFontName);
+        if ((*env)->ExceptionOccurred(env)) {
+            CFRelease(fds);
+            return;
+        }
+        (*env)->DeleteLocalRef(env, jFontName);
+    }
+    CFRelease(fds);
+JNI_COCOA_EXIT(env);
+}

samkenxstream/SAMkenxjetbrainsruntime

(cherry picked from commits a155760e94d989ce2584233fa955072e6ef72287, 42fa3e3d34e5f4a98bb8af18d82e7c22da143db5, 82048d1d7362485541ac504fae1a184a133b3782)

src/java.desktop/macosx/native/libawt_lwawt/font/AWTFont.m
@@ -3349,6 +3349,64 @@ Java_sun_awt_FontDescriptor_initIDs
 }
 #endif
 
+/*
+ * Class:     sun_font_CFont
+ * Method:    getCascadeList
+ * Signature: (JLjava/util/ArrayList;)V
+ */
+JNIEXPORT void JNICALL
+Java_sun_font_CFont_getCascadeList
+    (JNIEnv *env, jclass cls, jlong awtFontPtr, jobject arrayListOfString)
+{
+JNI_COCOA_ENTER(env);
+    jclass alc = (*env)->FindClass(env, "java/util/ArrayList");
+    if (alc == NULL) return;
+    jmethodID addMID = (*env)->GetMethodID(env, alc, "add", "(Ljava/lang/Object;)Z");
+    if (addMID == NULL) return;
+
+    CFIndex i;
+    AWTFont *awtFont = (AWTFont *)jlong_to_ptr(awtFontPtr);
+    NSFont* nsFont = awtFont->fFont;
+#ifdef DEBUG
+    CFStringRef base = CTFontCopyFullName((CTFontRef)nsFont);
+    NSLog(@"BaseFont is : %@", (NSString*)base);
+    CFRelease(base);
+#endif
+    bool anotherBaseFont = false;
+    if (awtFont->fFallbackBase != nil) {
+        nsFont = awtFont->fFallbackBase;
+        anotherBaseFont = true;
+    }
+    CTFontRef font = (CTFontRef)nsFont;
+    CFArrayRef codes = CFLocaleCopyPreferredLanguages();
+
+    CFArrayRef fds = CTFontCopyDefaultCascadeListForLanguages(font, codes);
+    CFRelease(codes);
+    CFIndex cnt = CFArrayGetCount(fds);
+    for (i= anotherBaseFont ? -1 : 0; i<cnt; i++) {
+        CFStringRef fontname;
+        if (i < 0) {
+            fontname = CTFontCopyPostScriptName(font);
+        } else {
+            CTFontDescriptorRef ref = CFArrayGetValueAtIndex(fds, i);
+            fontname = CTFontDescriptorCopyAttribute(ref, kCTFontNameAttribute);
+        }
+#ifdef DEBUG
+        NSLog(@"Font is : %@", (NSString*)fontname);
+#endif
+        jstring jFontName = (jstring)NSStringToJavaString(env, fontname);
+        CFRelease(fontname);
+        (*env)->CallBooleanMethod(env, arrayListOfString, addMID, jFontName);
+        if ((*env)->ExceptionOccurred(env)) {
+            CFRelease(fds);
+            return;
+        }
+        (*env)->DeleteLocalRef(env, jFontName);
+    }
+    CFRelease(fds);
+JNI_COCOA_EXIT(env);
+}
+
 static CFStringRef EMOJI_FONT_NAME = CFSTR("Apple Color Emoji");
 
 bool IsEmojiFont(CTFontRef font)